8736078d0ea9dbd40d72d667a48f0580d2025533
mmsatyadev <124125915+mmsatyadev@users.noreply.github.com>
2023-12-13 18:42:54 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+function submitForm() {
+    const firstName = document.getElementById('firstName').value;
+    const lastName = document.getElementById('lastName').value;
+    const dob = document.getElementById('dob').value;
+    const country = document.getElementById('country').value;
+    const gender = Array.from(document.querySelectorAll('input[name="gender"]:checked')).map(checkbox => checkbox.value).join(', ');
+    const profession = document.getElementById('profession').value;
+    const email = document.getElementById('email').value;
+    const mobile = document.getElementById('mobile').value;
+  
+    const popupResult = document.getElementById('popupResult');
+    popupResult.innerHTML = `
+      <p><strong>First Name:</strong> ${firstName}</p>
+      <p><strong>Last Name:</strong> ${lastName}</p>
+      <p><strong>Date of Birth:</strong> ${dob}</p>
+      <p><strong>Country:</strong> ${country}</p>
+      <p><strong>Gender:</strong> ${gender}</p>
+      <p><strong>Profession:</strong> ${profession}</p>
+      <p><strong>Email:</strong> ${email}</p>
+      <p><strong>Mobile Number:</strong> ${mobile}</p>
+    `;
+  
+    document.getElementById('popup').style.display = 'block';
+  }
+  
+  function closePopup() {
+    document.getElementById('popup').style.display = 'none';
+    resetForm();
+  }
+  
+  function resetForm() {
+    document.getElementById('surveyForm').reset();
+  }
+  
